254fd5767463cbd2aa909541e81fcf5e0c252598
Krzys Kobus <kobus.krzys@gmail.com>
2026-06-18 11:15:27 +0200

    home improvements

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit, ElementRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { TranslatePipe } from '../../shared/pipes/translate.pipe';
 
@@ -9,25 +9,4 @@ import { TranslatePipe } from '../../shared/pipes/translate.pipe';
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home implements OnInit {
-  constructor(private el: ElementRef) {}
-
-  ngOnInit(): void {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-          }
-        });
-      },
-      { threshold: 0.15 }
-    );
-
-    setTimeout(() => {
-      this.el.nativeElement.querySelectorAll('.reveal-up').forEach((el: Element) => {
-        observer.observe(el);
-      });
-    }, 0);
-  }
-}
+export class Home {}
